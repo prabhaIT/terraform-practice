455ddfc24ebb33857eb55023e81d989989216daa
prabhakar <drapj1227@gmail.com>
2025-10-30 13:20:25 +0530

    day-4

---
+++ day-4-stackfile-lock/main.tf
@@ -0,0 +1,36 @@
+# Create VPC
+resource "aws_vpc" "prabha_vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "prabha-vpc"
+  }
+}
+
+# Create Subnet
+resource "aws_subnet" "prabha_subnet" {
+  vpc_id                  = aws_vpc.prabha_vpc.id
+  cidr_block              = aws_vpc.prabha_vpc.cidr_block
+  
+  availability_zone       = "ap-south-1a"
+
+  tags = {
+    Name = "prabha-subnet"
+  }
+}
+ resource "aws_instance" "name" {
+    ami = var.ami_id
+    instance_type = var.type
+   
+ }
+ 
+ resource "aws_s3_bucket" "my_bucket" {
+  bucket = "prabha-terraform-s3-bucket"   # must be globally unique
+  
+
+  tags = {
+    Name        = "PrabhaS3Bucket"
+  
+  }
+}
+
---
+++ day-4-stackfile-lock/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_id = "ami-00af95fa354fdb788"
+type = "t3.micro"
+vpc_cidr = "10.0.0.0/16"
